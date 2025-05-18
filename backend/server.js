db2af30e8d505b50d5f81b047896c63064d7e198
patient <undefinedpatient@gmail.com>
2025-05-18 22:07:59 +0800

    Updated H1 & colorVars

--- backend/server.js
+++ backend/server.js
@@ -22,19 +22,8 @@ app.get('/health', (req, res) => {
   res.status(200).send(requestLog);
 });
 app.get('/auth', (req, res)=>{
-  const requestLog = {
-    timestamp: new Date().toISOString(),
-    method: req.method,
-    url: req.originalUrl,
-    headers: req.headers,
-    query: req.query,
-    body: req.body,
-    ip: req.ip,
-    protocol: req.protocol,
-    hostname: req.hostname,
-  };
-  console.log("Auth Checking From: "+ JSON.stringify(req.headers, null, 2));
-  res.status(200).send(requestLog);
+  console.log("\nAuth Checking From Accepted\n");
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
